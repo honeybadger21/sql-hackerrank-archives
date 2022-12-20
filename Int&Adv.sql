83a996d07a8bd0c8dcd2fcc0e9c5ba1bcf90d5cb
Ruchi Sharma <statsnotes21@gmail.com>
2022-12-19 19:33:01 -0600

    Update Int&Adv.sql

--- Int&Adv.sql
+++ Int&Adv.sql
@@ -11,3 +11,9 @@ CASE WHEN B1.P IS NULL THEN 'Root'
      WHEN B1.P IS NOT NULL THEN 'Inner'
 END AS NAME
 FROM BST B1 LEFT JOIN BST B2 ON B1.N = B2.P ORDER BY B1.N
+
+-- Problem 02: New Companies 
+SELECT C.company_code, C.founder, 
+       COUNT(DISTINCT E.lead_manager_code), COUNT(DISTINCT E.senior_manager_code), 
+       COUNT(DISTINCT E.manager_code), COUNT(DISTINCT E.employee_code)
+FROM company as C INNER JOIN employee as E USING(company_code) GROUP BY c.company_code, c.founder
